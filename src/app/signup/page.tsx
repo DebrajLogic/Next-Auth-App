e6cb54343ece9ddf04c7e1dd8b5d39d0bc6abbf2
DebrajLogic <debrajdey9696@gmail.com>
2024-04-01 15:49:57 -0400

    added verify email page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,13 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function SignupPage() {
+  const router = useRouter();
+
   const [user, setUser] = useState({ email: "", password: "", username: "" });
 
   const [buttonDisabled, setButtonDisabled] = useState(false);
@@ -14,13 +18,75 @@ function SignupPage() {
     try {
       setLoading(true);
       const response = await axios.post("/api/users/signup", user);
+      toast.success("Success");
       console.log("Signup success", response.data);
-    } catch (error) {
+      router.push("/login");
+    } catch (error: any) {
       console.log("Signup failed");
+      toast.error(error.message);
     }
   };
 
-  return <div>SignupPage</div>;
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Signing you up..." : "Sign Up"}</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black mb-4"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+        type="text"
+      />
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black mb-4"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+        type="text"
+      />
+      <hr />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black mb-4"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+        type="text"
+      />
+      <button
+        onClick={onSignUp}
+        className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 mb-4"
+        disabled={buttonDisabled}
+      >
+        Sign Up
+      </button>
+      <p>
+        Already Registered?{" "}
+        <Link className="text-blue-300" href="/login">
+          Login
+        </Link>
+      </p>
+    </div>
+  );
 }
 
 export default SignupPage;
--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function page() {
-  return <div>verify email page</div>;
+function VerifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+  };
+
+  useEffect(() => {
+    const urlToken = window.location.search.split("=")[1];
+    setToken(urlToken || "");
+  }, []);
+
+  useEffect(() => {
+    if (token.length > 0) {
+      verifyUserEmail();
+    }
+  }, [token]);
+
+  return <div>VerifyEmailPage</div>;
 }
 
-export default page;
+export default VerifyEmailPage;
